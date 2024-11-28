2f031722d53491271f62b52213c87073e10bd573
Ashutosh Bhargava <ashutumho@gmail.com>
2024-11-29 02:32:18 +0530

    Added build config like previous version

---
+++ AndroidStudio/SpeechToText/app/src/main/java/com/starseed/speechtotext/BuildConfig.java
@@ -0,0 +1,9 @@
+package com.starseed.speechtotext;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = false;
+
+    public static final String LIBRARY_PACKAGE_NAME = "com.starseed.speechtotext";
+
+    public static final String BUILD_TYPE = "release";
+}
